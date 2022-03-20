1efe61036b787b7e96a2cd275b37939997944851
great-sublime <97266297+great-sublime@users.noreply.github.com>
2022-03-20 17:45:52 -0600

    .

---
+++ Projects and codes/Java Codes/Practice.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    // This is a comment
+    /*
+     * Multiline
+     * Comment
+     */
+    System.out.println("Hello World");
+    // String
+    String name = "John";
+    System.out.println(name);
+    // Integers
+    int myNum = 15;
+    System.out.println(myNum);
+    // Integers with string
+    myNum = 15;
+    System.out.println(myNum);
+    // Change in variable int
+    myNum = 20; // myNum is now 20
+    System.out.println(myNum);
+
+  }
+}
